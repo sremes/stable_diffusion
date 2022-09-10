6f8cb5e222083dfc39f588d8d51baed3fe12974b
Sami Remes <sami.remes@gmail.com>
2022-09-10 16:39:15 +0300

    create a demo with fi->en MT and diffusers

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Stable Diffusion
+
+## Simple demo with Finnish prompts
+
+This demo accepts Finnish prompts. The prompts are machine translated by [`opus-mt-fi-en`](https://huggingface.co/Helsinki-NLP/opus-mt-fi-en) to English. 
+
+The translated prompt is fed into the [Stable Diffusion](https://huggingface.co/CompVis/stable-diffusion-v1-4) model, which is loaded in fp16 precision in order to be able to run on low-memory GPU. The model runs fine on e.g. a NVIDIA GTX 1060 6GB card; generating one image takes about one minute.
+
+## Running from WSL2
+
+NVIDIA drivers only need to be installed on Windows. CUDA environment can be installed according to [instructions from NVIDIA](https://docs.nvidia.com/cuda/wsl-user-guide/index.html).
+
+Add port forwarding and firewall rule for Windows from elevated Powershell prompt. The default gradio port is `7860`. 
+The IP address of the WSL2 Linux VM in this example is `172.30.136.24`.
+```powershell
+netsh interface portproxy add v4tov4 listenport=7860 listenaddress=0.0.0.0 connectport=7860 connectaddress=172.30.136.24
+netsh advfirewall firewall add rule name= "Open Port 7860" dir=in action=allow protocol=TCP localport=7860
+```
---
+++ demo.py
@@ -0,0 +1,36 @@
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, TranslationPipeline
+from PIL.Image import Image
+
+
+class StableDiffusionDemo:
+    translation_model_name: str = "Helsinki-NLP/opus-mt-fi-en"
+    diffusion_model_name: str = "CompVis/stable-diffusion-v1-4"
+    __name__: str = "stable_diffusion"
+    
+    def __init__(self):
+        # create translation pipeline from Finnish to English
+        tokenizer = AutoTokenizer.from_pretrained(self.translation_model_name)
+        translation_model = AutoModelForSeq2SeqLM.from_pretrained(self.translation_model_name)
+        self.translator = TranslationPipeline(translation_model, tokenizer)
+        # load the stable diffusion model
+        self.diffuser = StableDiffusionPipeline.from_pretrained(
+            self.diffusion_model_name, 
+            torch_dtype=torch.float16, 
+            revision="fp16", 
+            use_auth_token=True,
+        ).to("cuda")
+    
+    def __call__(self, prompt: str) -> Image:
+        with autocast("cuda"):
+            prompt = self.translator(prompt)[0]["translation_text"]
+            image = self.diffuser(prompt).images[0]
+        return image
+
+
+if __name__ == "__main__":
+    import gradio as gr
+    demo = gr.Interface(fn=StableDiffusionDemo(), inputs="text", outputs="image")
+    demo.launch(server_name="0.0.0.0")
